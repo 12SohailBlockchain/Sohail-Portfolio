fe9fc843c543946a904e0e736e0c040529fbe69b
Sohail Ahmed <sohailahmedlaghari21@gmail.com>
2023-05-29 11:49:22 +0500

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,133 @@
+import bwmap from './backgrounds/bw-map.jpeg';
+import nairobi from './backgrounds/nairobi.png';
+import whiteabstract from './backgrounds/white-abstract.png';
+import worldmap from './backgrounds/world-map.png';
+
+import logo from './logo/logo-black.png';
+import logotext from './logo/logo-text-black.png';
+
+import shaq from './personal/shaq.png';
+
+import blockchain from './icons/blockchain.png';
+import ux from './icons/ux.png';
+import prototyping from './icons/prototyping.png';
+import ethereum from './icons/ethereum.png';
+// import blockchain from './icons/blockchain.png'; web3 nodes Truffle.png, Node
+import web3 from './icons/web3.png';
+import nodes from './icons/nodes.png';
+import Truffle from './icons/Truffle.png';
+
+import nft from './icons/nft.png';
+import github from './icons/github.png';
+import close from './icons/close.png';
+import download from './icons/download.png';
+import downloadHover from './icons/downloadHover.png';
+import pineapple from './icons/pineapple.png';
+import pineappleHover from './icons/pineappleHover.png';
+import menu from './icons/menu.png';
+import resume from './icons/resume.png';
+import send from './icons/send.png';
+import sendHover from './icons/sendHover.png';
+
+import Ethereum from './tech/Ethereum.png';
+import docker from './tech/docker.png';
+// import figma from './tech/figma.png';
+import Node from './tech/Node.png';
+import git from './tech/git.png';
+import Blockchain from './tech/Blockchain.png';
+import Polygon from './tech/Polygon.png';
+import solana from './tech/solana.png';
+import NFT from './tech/NFT.png';
+
+import cardano from './tech/cardano.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import typescript from './tech/typescript.png';
+import graphql from './tech/graphql.png';
+import postgresql from './tech/postgresql.png';
+import Web3 from './tech/web3.png';
+import binance from './tech/binance.png';
+
+import coverhunt from './company/coverhunt.png';
+import dcc from './company/dcc.png';
+import kelhel from './company/kelhel.png';
+import Inaequo from './company/Inaequo.png';
+import ArhamSoft from './company/ArhamSoft.png';
+import CryptoExpress from './company/CryptoExpress.png';  //ArhamSoft, CryptoExpress, Tanbits, Grands, Argon Tech Inc, Cyber
+import Argon from './company/Argon.png';
+import Grands from './company/Grands.png';
+import Tanbits from './company/Tanbits.png';
+import Cyber from './company/Cyber.png';
+
+import komikult from './projects/komikult.png';
+import leaderboard from './projects/leaderboard.png';
+import math from './projects/math-magicians.png';
+import movie from './projects/movie-metro.png';
+import nyeusi from './projects/nyeusi.png';
+import space from './projects/space-hub.png';
+
+export {
+  bwmap,
+  nairobi,
+  whiteabstract,
+  worldmap,
+  logo,
+  logotext,
+  shaq,
+  blockchain,
+  web3,
+  nodes,
+  Truffle,
+  // blockchain,
+  ux,
+  prototyping,
+  ethereum,
+  nft,
+  github,
+  close,
+  download,
+  downloadHover,
+  pineapple,
+  pineappleHover,
+  menu,
+  resume,
+  send,
+  sendHover,
+  Ethereum,
+  cardano,
+  binance,
+  docker,
+  Node,
+  // figma,
+  git,
+  graphql,
+  Blockchain,
+  Polygon,
+  solana,
+  NFT,
+  nodejs,
+  postgresql,
+  Web3,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  ArhamSoft,
+  CryptoExpress,
+  Tanbits,
+  Grands,
+  Argon,
+  Inaequo,
+  Cyber,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,351 @@
+import {
+  ethereum,
+  blockchain,
+  nft,
+  web3,
+  nodes,
+  Truffle,
+  ux,
+  prototyping,
+  Polygon,
+  // typescript,
+  Blockchain,
+  Ethereum,
+  solana,
+  cardano,
+  binance,
+  NFT,
+  reactjs,
+  // redux,
+  // tailwind,
+  nodejs,
+  git,
+  Node,
+  // figma,
+  docker,
+  // postgresql,
+  Web3,
+  graphql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  // space,
+  // coverhunt,
+ 
+  // dcc,
+  // kelhel,
+  ArhamSoft,
+  CryptoExpress,
+  Grands,
+  Inaequo,
+  Tanbits,
+  Argon,
+  Cyber,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Ethereum Developer & Solidity Programming ',
+    icon: ethereum,
+  },
+  
+  {
+    title: 'Backend Developer & Blockchain ',
+    icon: blockchain,
+  },
+  {
+    title: 'Blockchain Architecture',
+    icon: ux,
+  },
+  {
+    title: 'EVM Support Chains  Ethereum, BSC, Polygon',
+    icon: nodes,
+  },
+  {
+    title: 'NFT MARKETPLACE & BLOCKCHAIN DEVELOPER',
+    icon: nft,
+  },
+  {
+    title: '  Libraries/APIs, Web3.js, Ether.js, web3auth,  Node.js, React',
+    icon: web3,
+  },
+  {
+    title: 'Frameworks, Hardhat, Truffle, • infura.io, IPFS Storage,  alchemy Blockchain',
+    icon: Truffle,
+  },
+  {
+    title: 'Software Prototyping  ',
+    icon: prototyping,
+
+  }
+];
+
+const technologies = [
+  {
+    name: 'Blockchain 5',
+    icon: Blockchain,
+  },
+  {
+    name: 'Ethereum',
+    icon: Ethereum,
+  },
+  // {
+  //   name: 'Polygon',
+  //   icon: Polygon,
+  // },
+  {
+    name: 'Polygon',
+    icon: Polygon,
+  },
+   {
+    name: 'solana',
+    icon: solana,
+  },
+  // {
+  //   name: 'React JS',
+  //   icon: reactjs,
+  // },
+   {
+    name: 'cardano',
+    icon: cardano,
+  },
+  // {
+  //   name: 'Redux Toolkit',
+  //   icon: redux,
+  // },
+  {
+    name: 'binance',
+    icon: binance,
+  },
+  // {
+  //   name: 'Tailwind CSS',
+  //   icon: tailwind,
+  // },
+  {
+    name: 'NFT',
+    icon: NFT,
+  },
+    {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'Web3',
+    icon: Web3,
+  },
+  {
+    name: 'graphql',
+    icon: graphql,
+  },
+  // {
+  //   name: 'postgresql',
+  //   icon: postgresql,
+  // },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'Node',
+    icon: Node,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Solidity - Developer',
+    company_name: 'Crypto Express Thailand Company:',
+    icon: CryptoExpress, //// ICON WAITING///  CryptoExpress
+    iconBg: '#333333',//// Colour////
+    date: 'AUG 2022 - JAN 2023',
+  },
+  {
+    title: 'Blockchain - Developer',
+    company_name: 'Argon Tech Lahore, Pakistan',
+    icon: Argon, //// ICON WAITING///
+    iconBg: '#333333', //// Colour////
+    date: 'JAN 2020 - FEB 2021',
+  },
+  {
+    title: 'Junior-Blockchain-Developer',
+    company_name: 'Arham Soft Lahore, Pakistan',
+    icon: ArhamSoft,//// ICON WAITING///
+    iconBg: '#333333', //// Colour////
+    date: 'FEB 2021 - MAY 2021',
+  },
+   {
+    title: 'Blockchain - Engineer',
+    company_name: 'Company Cyber Space Lahore, Pakistan',
+    icon: Cyber,//// ICON WAITING///
+    iconBg: '#333333', //// Colour////
+    date: 'JUN 2019 - MAY 2020',
+  },
+   {
+    title: 'Sr Software - Engineer',
+    company_name: 'Inaequo Solutions Karachi, Pakistan',
+    icon: Inaequo,//// ICON WAITING///
+    iconBg: '#333333', //// Colour////
+    date: 'JAN 2019 – MAY/2019',
+  },
+  {
+    title: 'Sr Software - Engineer Elixir -(Erlang)',
+    company_name: 'Tanbits Lahore,Pakistan',
+    icon: Tanbits,
+    iconBg: '#333333',
+    date: 'OCT 2020 - DEC 20202',
+  },
+  {
+    title: 'Full - Stack - Blockchain - Developer',
+    company_name: 'Grands Digital',
+    icon: Grands,
+    iconBg: '#333333',
+    date: 'MAR 2023 - Present',
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'NftMarketPlace',
+    // description: 'A comic characters list app that displays Marvel characters.',
+    description: 'NFT MarketPlace Project Build Using 3 chains Polygon,BSC,Ethereum',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: komikult,
+    repo: 'https://github.com/12SohailBlockchain',
+    demo: 'https://shaqdeff.github.io/KomiKult/',
+  },
+  {
+    id: 'project-2',
+    name: 'Pancakeswape project',
+    // description:
+    //   'A leaderboard list app that displays scores submitted by different players.',
+
+    description: 'Using Pancakeswape Froent modife the Ui and put our Dex Smart contracts BSC Live',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: leaderboard,
+    repo: 'https://github.com/12SohailBlockchain',
+    demo: 'https://shaqdeff.github.io/Leaderboard/',
+  },
+  {
+    id: 'project-3',
+    name: 'Medical Project',/// still name is remaing///
+    description: 'Eelectronic health records(EHR)System based In Private Blockchian Technolgy',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: math,
+    repo: 'https://github.com/12SohailBlockchain',
+    demo: 'https://inspiring-medovik-37d3b3.netlify.app/',
+  },
+  {
+    id: 'project-4',
+    name: 'Vehicle Management System',
+    // description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
+
+    description: 'Vehicle Management System Through The Blockchain Technolgy Using the Hyperledger Fabric, Hyperledger Composer',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: movie,
+    repo: 'https://github.com/12SohailBlockchain',
+    demo: 'https://movie-metro.netlify.app/',
+  },
+  {
+    id: 'project-5',
+    name: 'Dapps',
+    description:
+      'This is a demo concert website for a music festival called Nyeusi.',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: nyeusi,
+    repo: 'https://github.com/12SohailBlockchain',
+    demo: 'https://shaqdeff.github.io/Nyeusi-Fest-Site/',
+  },
+];
+
+export { services, technologies, experiences, projects };
